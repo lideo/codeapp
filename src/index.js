5997bc7cbcb865c730613773fb36a9f46a77f115
lideo <1573567+lideo@users.noreply.github.com>
2020-05-03 18:45:04 +0800

    Add a 3 second delay between changes in code and a preview refresh.

--- src/index.js
+++ src/index.js
@@ -5,31 +5,52 @@ import Editor from "./components/Editor";
 import Preview from "./components/Preview";
 
 function App() {
-  const [initialHtmlCode] = useState(localStorage.getItem("htmlCode") || "");
-  const [initialCssCode] = useState(localStorage.getItem("cssCode") || "");
-  const [initialJsCode] = useState(localStorage.getItem("jsCode") || "");
+  const htmlCodeRef = useRef(localStorage.getItem("codeapp:htmlCode") || "");
+  const cssCodeRef = useRef(localStorage.getItem("codeapp:cssCode") || "");
+  const jsCodeRef = useRef(localStorage.getItem("codeapp:jsCode") || "");
+  const timeoutRef = useRef(null);
+  const previewReload = useRef(3000);
 
-  const [htmlCode, setHtmlCode] = useState(initialHtmlCode);
-  const [cssCode, setCssCode] = useState(initialCssCode);
-  const [jsCode, setJsCode] = useState(initialJsCode);
+  const [htmlCode, setHtmlCode] = useState(htmlCodeRef.current);
+  const [cssCode, setCssCode] = useState(cssCodeRef.current);
+  const [jsCode, setJsCode] = useState(jsCodeRef.current);
+
+  function updatePreview(editor, setCodeCb) {
+    if (timeoutRef.current !== null) {
+      return;
+    }
+    timeoutRef.current = setTimeout(() => {
+      timeoutRef.current = null;
+
+      console.log(Date.now(), "updating...");
+
+      setCodeCb(editor.getValue());
+    }, previewReload.current);
+  }
 
   function handleHtmlChange(editor, data, value) {
-    setHtmlCode(value);
+    updatePreview(editor, setHtmlCode);
   }
 
   function handleCssChange(editor, data, value) {
-    setCssCode(value);
+    updatePreview(editor, setCssCode);
   }
 
   function handleJsChange(editor, data, value) {
-    setJsCode(value);
+    updatePreview(editor, setJsCode);
   }
 
   useEffect(() => {
-    localStorage.setItem("htmlCode", htmlCode);
-    localStorage.setItem("cssCode", cssCode);
-    localStorage.setItem("jsCode", jsCode);
-  }, [htmlCode, cssCode, jsCode]);
+    localStorage.setItem("codeapp:htmlCode", htmlCode);
+  }, [htmlCode]);
+
+  useEffect(() => {
+    localStorage.setItem("codeapp:cssCode", cssCode);
+  }, [cssCode]);
+
+  useEffect(() => {
+    localStorage.setItem("codeapp:jsCode", jsCode);
+  }, [jsCode]);
 
   return (
     <React.Fragment>
@@ -38,13 +59,17 @@ function App() {
           mode="xml"
           autofocus={true}
           onChange={handleHtmlChange}
-          value={initialHtmlCode}
+          value={htmlCodeRef.current}
         />
-        <Editor mode="css" onChange={handleCssChange} value={initialCssCode} />
+        <Editor
+          mode="css"
+          onChange={handleCssChange}
+          value={cssCodeRef.current}
+        />
         <Editor
           mode="javascript"
           onChange={handleJsChange}
-          value={initialJsCode}
+          value={jsCodeRef.current}
         />
         <Preview cssCode={cssCode} htmlCode={htmlCode} jsCode={jsCode} />
       </div>
