371474d0465f7a86074747d505f4a265f0134b2d
lideo <1573567+lideo@users.noreply.github.com>
2020-05-12 12:28:19 +0800

    Move grid resizing logic to Grid component

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,37 +1,187 @@
-import React, { Children } from "react";
+import React, { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 
 import Resizer from "./Resizer";
 import Cell from "./Cell";
 
+function debounce(fn, ms) {
+  let timer;
+  return (_) => {
+    clearTimeout(timer);
+    timer = setTimeout((_) => {
+      timer = null;
+      fn.apply(this, arguments);
+    }, ms);
+  };
+}
+
+const noResizer = {
+  isResizing: false,
+  type: null,
+  id: null,
+};
+
 function Grid({
-  children,
-  className = "",
-  onMouseUpCallback = () => {},
-  onMouseMoveCallback = () => {},
+  htmlEditor = "",
+  cssEditor = "",
+  jsEditor = "",
+  preview = "",
 }) {
-  function getTemplateAreas(areas) {
-    const rows = areas.map((row) => row.join(" "));
-    const columns = rows.join("' '");
-    return "'" + columns + "'";
+  const resizer = useRef(noResizer);
+
+  useEffect(() => {
+    const debouncedHandleResize = debounce(() => {
+      resetSizes();
+    }, 100);
+    window.addEventListener("resize", debouncedHandleResize);
+
+    return () => window.removeEventListener("resize", debouncedHandleResize);
+  }, []);
+
+  function setResizeCursor(cursor) {
+    document.querySelector("body").style.cursor = cursor;
+  }
+
+  function handleStartResize(event, resizerType, resizerId) {
+    resizer.current = {
+      isResizing: true,
+      type: resizerType,
+      id: resizerId,
+    };
+
+    const cursor = resizerType === "horizontal" ? "row-resize" : "col-resize";
+    setResizeCursor(cursor);
+
+    disablePreviewArea();
+  }
+
+  function handleEndResize(event) {
+    resizer.current = noResizer;
+
+    setResizeCursor("auto");
+
+    enablePreviewArea();
+  }
+
+  function disablePreviewArea() {
+    document.getElementById("previewArea").style.zIndex = -1;
+  }
+  function enablePreviewArea() {
+    document.getElementById("previewArea").style.zIndex = "initial";
+  }
+
+  function computeSizes(
+    resizerElementSize,
+    prevIndex,
+    nextIndex,
+    cursorPosition,
+    prevOffset,
+    sizes
+  ) {
+    sizes = sizes.map((size) => parseFloat(size));
+
+    let sizesNew = JSON.parse(JSON.stringify(sizes));
+    let diff =
+      cursorPosition - prevOffset - resizerElementSize - sizes[prevIndex];
+
+    sizesNew[prevIndex] = sizesNew[prevIndex] + diff;
+    sizesNew[nextIndex] = sizesNew[nextIndex] - diff;
+    sizesNew = sizesNew.map((size) => size.toString() + "px").join(" ");
+
+    return sizesNew;
+  }
+
+  function resetSizes() {
+    const allGrids = document.querySelectorAll(".grid-container");
+
+    allGrids.forEach((grid) => {
+      grid.style.gridTemplateRows = "";
+      grid.style.gridTemplateColumns = "";
+    });
+  }
+
+  function handleResize(event) {
+    if (resizer.current.isResizing) {
+      const resizerElement = document.getElementById(resizer.current.id);
+      const prevElement = resizerElement.previousSibling;
+      const nextElement = resizerElement.nextSibling;
+      const parentGrid = resizerElement.parentNode;
+      const areas = getComputedStyle(parentGrid)
+        .gridTemplateAreas.split('"')
+        .join("")
+        .split(" ");
+
+      if (resizer.current.type === "horizontal") {
+        let sizes = getComputedStyle(parentGrid).gridTemplateRows.split(" ");
+
+        const sizesNew = computeSizes(
+          resizerElement.clientHeight,
+          areas.indexOf(prevElement.id),
+          areas.indexOf(nextElement.id),
+          event.clientY,
+          prevElement.offsetTop,
+          sizes
+        );
+
+        parentGrid.style.gridTemplateRows = sizesNew;
+      } else {
+        // Vertical
+        let sizes = getComputedStyle(parentGrid).gridTemplateColumns.split(" ");
+
+        const sizesNew = computeSizes(
+          resizerElement.clientWidth,
+          areas.indexOf(prevElement.id),
+          areas.indexOf(nextElement.id),
+          event.clientX,
+          prevElement.offsetLeft,
+          sizes
+        );
+
+        parentGrid.style.gridTemplateColumns = sizesNew;
+      }
+
+      event.preventDefault();
+    }
   }
 
   return (
     <div
-      className={`grid-container ${className}`}
-      onMouseUp={onMouseUpCallback}
-      onMouseMove={onMouseMoveCallback}
+      className="grid-container main-grid"
+      onMouseUp={handleEndResize}
+      onMouseMove={handleResize}
     >
-      {children}
+      <Cell id="editorArea">
+        <div className="grid-container editor-grid">
+          <Cell id="htmlEditor">{htmlEditor}</Cell>
+          <Resizer
+            id="resizer2"
+            type="horizontal"
+            onMouseDownCallback={handleStartResize}
+          />
+          <Cell id="cssEditor">{cssEditor}</Cell>
+          <Resizer
+            id="resizer3"
+            type="horizontal"
+            onMouseDownCallback={handleStartResize}
+          />
+          <Cell id="jsEditor">{jsEditor}</Cell>
+        </div>
+      </Cell>
+      <Resizer
+        id="resizer1"
+        type="vertical"
+        onMouseDownCallback={handleStartResize}
+      />
+      <Cell id="previewArea">{preview}</Cell>
     </div>
   );
 }
 
 Grid.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-  onMouseUpCallback: PropTypes.func,
-  onMouseMoveCallback: PropTypes.func,
+  htmlEditor: PropTypes.object,
+  cssEditor: PropTypes.object,
+  jsEditor: PropTypes.object,
+  preview: PropTypes.object,
 };
 
-export { Grid, Resizer, Cell };
+export default Grid;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useRef } from "react";
 import ReactDOM from "react-dom";
 
-import { Grid, Resizer, Cell } from "./components/Grid";
+import Grid from "./components/Grid";
 import Editor from "./components/Editor";
 import Preview from "./components/Preview";
 
@@ -16,17 +16,6 @@ const initialValues = {
 };
 
 const previewReloadDelay = 3000;
-
-function debounce(fn, ms) {
-  let timer;
-  return (_) => {
-    clearTimeout(timer);
-    timer = setTimeout((_) => {
-      timer = null;
-      fn.apply(this, arguments);
-    }, ms);
-  };
-}
 
 function App() {
   const [htmlCode, setHtmlCode] = useState(initialValues.htmlCode);
@@ -60,176 +49,45 @@ function App() {
     localStorage.setItem(jsCodeKey, jsCode);
   }, [htmlCode, cssCode, jsCode]);
 
-  useEffect(() => {
-    const debouncedHandleResize = debounce(() => {
-      resetSizes();
-    }, 100);
-    window.addEventListener("resize", debouncedHandleResize);
+  const htmlEditor = (
+    <Editor
+      mode="xml"
+      autofocus={true}
+      onChange={handleChange}
+      value={initialValues.htmlCode}
+      ref={htmlCodeEditorRef}
+    />
+  );
 
-    return () => window.removeEventListener("resize", debouncedHandleResize);
-  }, []);
+  const cssEditor = (
+    <Editor
+      mode="css"
+      onChange={handleChange}
+      value={initialValues.cssCode}
+      ref={cssCodeEditorRef}
+    />
+  );
 
-  const noResizer = {
-    isResizing: false,
-    type: null,
-    id: null,
-  };
+  const jsEditor = (
+    <Editor
+      mode="javascript"
+      onChange={handleChange}
+      value={initialValues.jsCode}
+      ref={jsCodeEditorRef}
+    />
+  );
 
-  const resizer = useRef(noResizer);
-
-  function setResizeCursor(cursor) {
-    document.querySelector("body").style.cursor = cursor;
-  }
-
-  function handleStartResize(event, resizerType, resizerId) {
-    resizer.current = {
-      isResizing: true,
-      type: resizerType,
-      id: resizerId,
-    };
-
-    const cursor = resizerType === "horizontal" ? "row-resize" : "col-resize";
-    setResizeCursor(cursor);
-
-    document.getElementById("previewArea").style.zIndex = -1;
-  }
-
-  function handleEndResize(event) {
-    resizer.current = noResizer;
-
-    setResizeCursor("auto");
-
-    document.getElementById("previewArea").style.zIndex = "initial";
-  }
-
-  function computeSizes(
-    resizerElementSize,
-    prevIndex,
-    nextIndex,
-    cursorPosition,
-    prevOffset,
-    sizes
-  ) {
-    sizes = sizes.map((size) => parseFloat(size));
-
-    let sizesNew = JSON.parse(JSON.stringify(sizes));
-    let diff =
-      cursorPosition - prevOffset - resizerElementSize - sizes[prevIndex];
-
-    sizesNew[prevIndex] = sizesNew[prevIndex] + diff;
-    sizesNew[nextIndex] = sizesNew[nextIndex] - diff;
-    sizesNew = sizesNew.map((size) => size.toString() + "px").join(" ");
-
-    return sizesNew;
-  }
-
-  function resetSizes() {
-    const allGrids = document.querySelectorAll(".grid-container");
-
-    allGrids.forEach((grid) => {
-      grid.style.gridTemplateRows = "";
-      grid.style.gridTemplateColumns = "";
-    });
-  }
-
-  function handleResize(event) {
-    if (resizer.current.isResizing) {
-      const resizerElement = document.getElementById(resizer.current.id);
-      const prevElement = resizerElement.previousSibling;
-      const nextElement = resizerElement.nextSibling;
-      const parentGrid = resizerElement.parentNode;
-      const areas = getComputedStyle(parentGrid)
-        .gridTemplateAreas.split('"')
-        .join("")
-        .split(" ");
-
-      if (resizer.current.type === "horizontal") {
-        let sizes = getComputedStyle(parentGrid).gridTemplateRows.split(" ");
-
-        const sizesNew = computeSizes(
-          resizerElement.clientHeight,
-          areas.indexOf(prevElement.id),
-          areas.indexOf(nextElement.id),
-          event.clientY,
-          prevElement.offsetTop,
-          sizes
-        );
-
-        parentGrid.style.gridTemplateRows = sizesNew;
-      } else {
-        // Vertical
-        let sizes = getComputedStyle(parentGrid).gridTemplateColumns.split(" ");
-
-        const sizesNew = computeSizes(
-          resizerElement.clientWidth,
-          areas.indexOf(prevElement.id),
-          areas.indexOf(nextElement.id),
-          event.clientX,
-          prevElement.offsetLeft,
-          sizes
-        );
-
-        parentGrid.style.gridTemplateColumns = sizesNew;
-      }
-
-      event.preventDefault();
-    }
-  }
+  const preview = (
+    <Preview cssCode={cssCode} htmlCode={htmlCode} jsCode={jsCode} />
+  );
 
   return (
     <Grid
-      className="main-grid"
-      onMouseUpCallback={handleEndResize}
-      onMouseMoveCallback={handleResize}
-    >
-      <Cell id="editorArea">
-        <Grid className="editor-grid">
-          <Cell id="htmlEditor">
-            <Editor
-              mode="xml"
-              autofocus={true}
-              onChange={handleChange}
-              value={initialValues.htmlCode}
-              ref={htmlCodeEditorRef}
-            />
-          </Cell>
-          <Resizer
-            id="resizer2"
-            type="horizontal"
-            onMouseDownCallback={handleStartResize}
-          />
-          <Cell id="cssEditor">
-            <Editor
-              mode="css"
-              onChange={handleChange}
-              value={initialValues.cssCode}
-              ref={cssCodeEditorRef}
-            />
-          </Cell>
-          <Resizer
-            id="resizer3"
-            type="horizontal"
-            onMouseDownCallback={handleStartResize}
-          />
-          <Cell id="jsEditor">
-            <Editor
-              mode="javascript"
-              onChange={handleChange}
-              value={initialValues.jsCode}
-              ref={jsCodeEditorRef}
-            />
-          </Cell>
-        </Grid>
-      </Cell>
-      <Resizer
-        id="resizer1"
-        type="vertical"
-        onMouseDownCallback={handleStartResize}
-      />
-      <Cell id="previewArea">
-        <Preview cssCode={cssCode} htmlCode={htmlCode} jsCode={jsCode} />
-      </Cell>
-    </Grid>
+      htmlEditor={htmlEditor}
+      cssEditor={cssEditor}
+      jsEditor={jsEditor}
+      preview={preview}
+    />
   );
 }
 
